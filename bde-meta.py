ccb77f8e49c6db58359c5decef083159b6a9b810
Tim Douglas <tdouglas4@bloomberg.net>
2013-11-23 12:32:26 -0500

    Add CXXFLAGS support, full dependency builds

--- bde-meta.py
+++ bde-meta.py
@@ -76,9 +76,43 @@ def get_package_includes(group, packages):
         incs.append('-I' + os.path.join(resolve_group(group), p))
     return ' '.join(incs)
 
+def get_group_library(group):
+    return os.path.join('out', 'libs', 'lib{}.a'.format(group))
+
+def get_group_components(group):
+    deps           = get_group_dependencies(group)
+    group_includes = get_group_includes(deps)
+
+    components = {}
+    for p in get_packages(group):
+        includes = group_includes + ' ' + get_package_includes(group,
+                set([p]) | get_package_dependencies(group, p))
+        cs = get_components(group, p)
+        for c in cs:
+            components[c] = {
+                'cpp': os.path.join(resolve_group(group), p, c + '.cpp'),
+                'object': os.path.join('out', 'objs', c + '.o'),
+                'includes': includes,
+            }
+    return components
+
+def print_group_targets(group):
+    lib        = get_group_library(group)
+    components = get_group_components(group)
+    objects    = ' '.join(c['object'] for c in components.values())
+
+    print('''{lib}: {objects}
+	ar -qs {lib} {objects}
+'''.format(lib=lib, objects=objects))
+
+    for c in components.values():
+        print('''{obj}: {cpp} | out/objs
+	$(CXX) -c $(CXXFLAGS) {includes} {cpp} -o {obj}
+'''.format(obj=c['object'], cpp=c['cpp'], includes=c['includes']))
+
 def main():
     parser = argparse.ArgumentParser();
-    parser.add_argument('action', choices={'cflags', 'mkmk'})
+    parser.add_argument('action', choices={'cflags', 'deps', 'mkmk'})
     parser.add_argument('group', type=str)
     args = parser.parse_args()
 
@@ -86,31 +120,27 @@ def main():
     if args.action == 'cflags':
         print(get_group_includes(
             set([group]) | get_group_dependencies(group)))
+    elif args.action == 'deps':
+        print('\n'.join(list(get_group_dependencies(group))))
     elif args.action == 'mkmk':
-        lib = os.path.join('out', 'libs', 'lib{}.a'.format(group))
-        group_includes = get_group_includes(get_group_dependencies(group))
+        lib      = get_group_library(group)
+        deps     = get_group_dependencies(group)
+        dep_libs = (get_group_library(g) for g in deps)
 
-        components = {}
-        for p in get_packages(group):
-            includes = group_includes + ' ' + get_package_includes(group,
-                    set([p]) | get_package_dependencies(group, p))
-            cs = get_components(group, p)
-            for c in cs:
-                components[c] = {
-                    'cpp': os.path.join(resolve_group(group), p, c + '.cpp'),
-                    'object': os.path.join('out', 'objs', c + '.o'),
-                    'includes': includes,
-                }
+        components = get_group_components(group)
         objects = ' '.join(c['object'] for c in components.values())
 
-        print('''{lib}: out/libs {objects}
-	ar -qs {lib} {objects}
-'''.format(lib=lib, objects=objects))
+        print('.PHONY: all deps\n')
+        print('CXXFLAGS+=-Dunix\n')
 
-        for c in components.values():
-            print('''{obj}: out/objs
-	$(CXX) -c -Dunix {includes} {cpp} -o {obj}
-'''.format(obj=c['object'], cpp=c['cpp'], includes=c['includes']))
+        print_group_targets(group)
+
+        print('all: {lib} deps\n'.format(lib=lib))
+
+        print('deps: ' + ' '.join(list(dep_libs)) + '\n')
+
+        for g in deps:
+            print_group_targets(g)
 
         print('''out/libs:
 	mkdir -p out/libs
