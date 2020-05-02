9352ce7ca602aaab05b9fdcb570ac143f0a1fe01
Masud Rahman <mail@masudrahman.com>
2020-05-01 21:59:48 -0400

    setup.py: bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bdemeta',
-      version='0.39',
+      version='0.40',
       description='Build and test BDE-style code',
       url='https://github.com/frutiger/bdemeta',
       author='Masud Rahman',
